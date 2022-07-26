918c6fffc232973ae6671ffcad72ec85e78239f9
Lilly Thomas <lilly@developmentseed.org>
2022-07-26 13:05:44 -0700

    add strings and ndwi expand dims

--- code/utils/mask_ndwi_nir_blue.py
+++ code/utils/mask_ndwi_nir_blue.py
@@ -24,22 +24,23 @@ def mask_ndwi_nir_blue(image_rgb, image_nir, image_pred, mask_type='nir'):
         img_pred (nd.array): The prediction image array, masked as needed.
     """
     if 3 in np.unique(image_pred):
-        if mask_type == ndwi:
+        if mask_type == 'ndwi':
             r,g,b = np.dsplit(image_nominal,image_nominal.shape[-1])
             
-            img_ndwi = ((g-image_nir))/((g+image_nir))
+            image_ndwi = ((g-image_nir))/((g+image_nir))
             ndwi_thresh = image_ndwi.max() * 0.1
             if ndwi_thresh < image_ndwi.min():
                 range_ndwi = image_ndwi.max() - image_ndwi.min()
                 ndwi_thresh = image_ndwi.max() - (0.9 * range_ndwi) 
+            image_ndwi = np.expand_dims(image_ndwi, axis=2)
             img_pred = mask(img_ndwi, image_pred, ndwi_thresh)
-        elif mask_type == nir: 
+        elif mask_type == 'nir': 
             nirthresh = image_nir.max() * 0.1
             if nirthresh < image_nir.min():
                 range_nir = image_nir.max() - image_nir.min()
                 nir_thresh = image_nir.max() - (0.9 * range_nir)
             img_pred = mask(image_nir, img_pred, nir_thresh)
-        elif mask_type == blue:
+        elif mask_type == 'blue':
             r,g,b = np.dsplit(image_nominal,image_nominal.shape[-1])
             blue_thresh = b.max() * 0.1
             if blue_thresh < b.min():
