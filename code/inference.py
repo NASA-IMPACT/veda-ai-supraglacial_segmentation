f2294042166833004866094956fc7c4cd18bd216
Lilly Thomas <lilly@developmentseed.org>
2022-08-09 15:58:34 -1000

    refactoring

--- code/inference.py
+++ code/inference.py
@@ -27,7 +27,7 @@ from tensorflow.python.keras import backend as K
 from tf_explain.callbacks.activations_visualization import ActivationsVisualizationCallback
 from tqdm.notebook import tqdm
 
-from utils import read_data
+from utils import model_utils, data_utils
 
 tfds.disable_progress_bar()
 
@@ -46,11 +46,13 @@ IMG_DIR = os.path.join(ROOT_DIR, datasets['icebridge_image_dir/'])
 LABEL_DIR = os.path.join(ROOT_DIR,datasets['icebridge_label_dir/']) 
 
 # input image shape
-IMG_SHAPE = (96, 96, 3)
+IMG_SHAPE = datasets['image_shape']
 # batch size for model
-BATCH_SIZE = 8
-# number of epochs completed for model training
-EPOCHS = 84
+BATCH_SIZE =  datasets['batch_size']
+# set number of model output channels to the number of classes (including background)
+OUTPUT_CHANNELS = datasets['output_channels']
+# number of epochs completed for model training using early stopping
+EPOCHS = 60 
 
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
@@ -70,96 +72,9 @@ if os.path.isfile(fn) for fn in [x_test_filenames_partition_fn, y_test_filenames
     x_test_filenames = [line.strip() for line in open(x_test_filenames_partition_fn, 'r')]
     y_test_filenames = [line.strip() for line in open(y_test_filenames_partition_fn, 'r')]
 else:
-    test_list, x_test_filenames, y_test_filenames = read_data.get_test_lists_planetscope(IMG_DIR, LABEL_DIR)
+    test_list, x_test_filenames, y_test_filenames = data_utils.get_test_lists_planetscope(IMG_DIR, LABEL_DIR)
 
 print("!!!!! number of images: ", len(x_test_filenames))
-
-# Function for reading the tiles into TensorFlow tensors 
-# See TensorFlow documentation for explanation of tensor: https://www.tensorflow.org/guide/tensor
-def _process_pathnames(fname, label_path):
-    # We map this function onto each pathname pair 
-    print("FILENAME: ", fname)
-    img_str = tf.io.read_file(fname)
-    img = tf.image.decode_png(img_str, channels=3)
-
-    label_img_str = tf.io.read_file(label_path)
-
-    # These are png images so they return as (num_frames, h, w, c)
-    label_img = tf.image.decode_png(label_img_str, channels=1)
-    # The label image should have any values between 0 and 8, indicating pixel wise
-    # foreground class or background (0). We take the first channel only. 
-    label_img = label_img[:, :, 0]
-    label_img = tf.expand_dims(label_img, axis=-1)
-    return img, label_img
-
-# Function to augment the data with horizontal flip
-def flip_img_h(horizontal_flip, tr_img, label_img):
-    if horizontal_flip:
-      flip_prob = tf.random.uniform([], 0.0, 1.0)
-      tr_img, label_img = tf.cond(tf.less(flip_prob, 0.5),
-                                  lambda: (tf.image.flip_left_right(tr_img), tf.image.flip_left_right(label_img)),
-                                  lambda: (tr_img, label_img))
-    return tr_img, label_img
-
-# Function to augment the data with vertical flip
-def flip_img_v(vertical_flip, tr_img, label_img):
-    if vertical_flip:
-      flip_prob = tf.random.uniform([], 0.0, 1.0)
-      tr_img, label_img = tf.cond(tf.less(flip_prob, 0.5),
-                                  lambda: (tf.image.flip_up_down(tr_img), tf.image.flip_up_down(label_img)),
-                                  lambda: (tr_img, label_img))
-    return tr_img, label_img
-
-# Function to augment the images and labels
-def _augment(img,
-             label_img,
-             resize=None,  # Resize the image to some size e.g. [256, 256]
-             scale=None,  # Scale image e.g. 1 / 255.
-             horizontal_flip=False,
-             vertical_flip=False): 
-    if resize is not None:
-        # Resize both images
-        label_img = tf.image.resize(label_img, resize)
-        img = tf.image.resize(img, resize)
-  
-    img, label_img = flip_img_h(horizontal_flip, img, label_img)
-    img, label_img = flip_img_v(vertical_flip, img, label_img)
-    img = tf.cast(img, tf.float32) 
-    if scale is not None:
-        img = tf.cast(img, tf.float32) * scale
-        #img = tf.keras.layers.Rescaling(scale=scale, offset=-1)
-    #label_img = tf.cast(label_img, tf.float32) * scale
-    #print("tensor: ", tf.unique(tf.keras.backend.print_tensor(label_img)))
-    return img, label_img
-
-# Main function to tie all of the above four dataset processing functions together 
-def get_baseline_dataset(filenames, 
-                         labels,
-                         preproc_fn=functools.partial(_augment),
-                         threads=5, 
-                         batch_size=BATCH_SIZE,
-                         shuffle=True):           
-    num_x = len(filenames)
-    # Create a dataset from the filenames and labels
-    print("BSL: ", filenames[0:10], labels[0:10])
-    dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-    print("dataset: ", dataset)
-    # Map our preprocessing function to every element in our dataset, taking
-    # advantage of multithreading
-    dataset = dataset.map(_process_pathnames, num_parallel_calls=threads)
-    if preproc_fn.keywords is not None and 'resize' not in preproc_fn.keywords:
-        assert BATCH_SIZE == 1, "Batching images must be of the same size"
-
-    dataset = dataset.map(preproc_fn, num_parallel_calls=threads)
-  
-    if shuffle:
-        dataset = dataset.shuffle(num_x)
-  
-  
-    # It's necessary to repeat our data for all epochs 
-    dataset = dataset.repeat().batch(BATCH_SIZE)
-    return dataset
-
 
 # dataset configuration for testing
 test_cfg = {
@@ -171,7 +86,7 @@ test_preprocessing_fn = functools.partial(_augment, **test_cfg)
 print(x_test_filenames[0:10])
 print(y_test_filenames[0:10])
 
-test_ds = get_baseline_dataset(x_test_filenames,
+test_ds = model_utils.get_baseline_dataset(x_test_filenames,
                               y_test_filenames, 
                               preproc_fn=test_preprocessing_fn,
                               batch_size=BATCH_SIZE)
@@ -201,7 +116,7 @@ r_choices = np.random.choice(num_foreground_examples, 1) #num_foreground_example
 for i in range(0, display_num * 2, 2):
     img_num = r_choices[i // 2]
 
-temp_ds = get_baseline_dataset(foreground_list_x[img_num:img_num+1], 
+temp_ds = model_utils.get_baseline_dataset(foreground_list_x[img_num:img_num+1], 
                                foreground_list_y[img_num:img_num+1],
                                preproc_fn=test_preprocessing_fn,
                                batch_size=1,
@@ -228,11 +143,6 @@ else:
     print("inferencing from in memory model")
 
 
-def create_mask(pred_mask):
-    pred_mask = tf.argmax(pred_mask, axis=-1)
-    pred_mask = pred_mask[..., tf.newaxis]
-    return pred_mask[0]
-
 def get_predictions(image= None, dataset=None, num=1):
     if image is None and dataset is None:
         return ValueError("At least one of image or dataset must not be None.")
@@ -241,7 +151,7 @@ def get_predictions(image= None, dataset=None, num=1):
             pred_mask = model.predict(image)
             return pred_mask
     else:
-        pred_mask = create_mask(model.predict(image[tf.newaxis, ...]))
+        pred_mask = model_utils.create_mask(model.predict(image[tf.newaxis, ...]))
         pred_mask = tf.keras.backend.eval(pred_mask)
         return pred_mask
 
