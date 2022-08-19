b97e43bc0ff406845dd3a95fb6f1948c6251374c
Lilly Thomas <lilly@developmentseed.org>
2022-08-19 13:14:05 +0200

    refactoring

--- code/utils/mask_ndwi_nir_blue.py
+++ code/utils/mask_ndwi_nir_blue.py
@@ -10,6 +10,16 @@ def mask(img_thresh, img_pred, thresh):
     img_pred[np.where((img_pred == 4) & (img_thresh >= thresh))] = 0
     return img_pred
 
+def process(image_arr, img_pred):
+    thresh = image_arr.max() * 0.1 
+    if thresh  < image_arr.min():
+        range = image_arr.max() - image_arr.min()
+        thresh = image_arr.max() - (0.9 * range) 
+    image_arr = np.expand_dims(image_arr, axis=2)
+    img_pred = mask(b, img_pred, thresh)
+    return img_pred
+
+
 def mask_ndwi_nir_blue(image_rgb, image_nir, image_pred, mask_type='nir'):
     """Takes a [r,g,b] image (pre-histogram matching) and masks areas of its prediction counterpart where 
     pixels are detected as melt pond but do not exceed a certain intensity in the threshold channel.
@@ -24,30 +34,14 @@ def mask_ndwi_nir_blue(image_rgb, image_nir, image_pred, mask_type='nir'):
         img_pred (nd.array): The prediction image array, masked as needed.
     """
     if 3 in np.unique(image_pred):
+        r,g,b = np.dsplit(image_nominal,image_nominal.shape[-1])
         if mask_type == 'ndwi':
-            r,g,b = np.dsplit(image_nominal,image_nominal.shape[-1])
             image_ndwi = ((g-image_nir))/((g+image_nir))
-            ndwi_thresh = image_ndwi.max() * 0.1
-            if ndwi_thresh < image_ndwi.min():
-                range_ndwi = image_ndwi.max() - image_ndwi.min()
-                ndwi_thresh = image_ndwi.max() - (0.9 * range_ndwi) 
-            image_ndwi = np.expand_dims(image_ndwi, axis=2)
-            img_pred = mask(img_ndwi, image_pred, ndwi_thresh)
+            image_pred = process(image_ndwi, image_pred)
         elif mask_type == 'nir': 
-            nirthresh = image_nir.max() * 0.1 
-            if nirthresh < image_nir.min():
-                range_nir = image_nir.max() - image_nir.min()
-                nir_thresh = image_nir.max() - (0.9 * range_nir)
-            image_nir = np.expand_dims(image_nir, axis=2)
-            img_pred = mask(image_nir, img_pred, nir_thresh)
+            image_pred = process(image_nir, image_pred)
         elif mask_type == 'blue':
-            r,g,b = np.dsplit(image_nominal,image_nominal.shape[-1])
-            blue_thresh = b.max() * 0.1
-            if blue_thresh < b.min():
-                range_blue = b.max() - b.min()
-                blue_thresh = b.max() - (0.9 * range_blue) 
-            img_pred = np.expand_dims(image_pred, axis=2)
-            img_pred = mask(b, img_pred, blue_thresh)
+            image_pred = process(b, image_pred)
         else:
             img_pred = image_pred
     else:
